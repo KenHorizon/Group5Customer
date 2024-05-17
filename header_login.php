22f70a6f4c28f5ecc34332b13fc29053f5ecfd21
KenHorizon <vincentruales845@gmail.com>
2024-05-17 22:02:47 +0800

    Fix not highlighting buttons

--- header_login.php
+++ header_login.php
@@ -4,13 +4,20 @@
             <li class="icon-texts"><a class="button" href="member_list.php" id="memberList"><span class="material-icons">list</span>Member List</a></li>
             <li><a class="button" href="membership.php" id="subscription"><span class="material-icons">rocket</span>Subscription</a></li>
             <li class="dropdown" style="float: right;">
-                <a class="account-header" id="account"><img class="avatar" src=<?php echo $user->user()['profile']; ?>><?php echo $user->account()['username']; ?></a>
+                <a class="account-header" id="account">
+                    <div class="group-box-rows">
+                        <img class="avatar" src=<?php echo $user->user()['profile'] == null ? "assets/img/default_pfp.jpeg" : $user->user()['profile']; ?>>
+                        <span class="account-header-text">
+                            <?php echo $user->account()['name']; ?>
+                        </span>
+                    </div>
+                </a>
                 <div class="dropdown-content">
                     <a class="account-header-button" href="account.php" id="account">
                         <div class="group-box-rows">
-                            <img class="avatar" src=<?php echo $user->user()['profile']; ?>>
+                            <img class="avatar" src=<?php echo $user->user()['profile'] == null ? "assets/img/default_pfp.jpeg" : $user->user()['profile']; ?>>
                             <span class="account-header-text">
-                                <?php echo $user->account()['username']; ?>
+                                <?php echo $user->account()['name']; ?>
                             </span>
                         </div>
                     </a>
