b0578c2d9130b7b81f07bf17270361acc18e2d2a
KenHorizon <vincentruales845@gmail.com>
2024-06-01 17:12:23 +0800

    Initial Commit

--- assets/database/Group5Database.sql
+++ assets/database/Group5Database.sql
@@ -1,47 +1,47 @@
-CREATE TABLE `user` (
-  `uuid` integer PRIMARY KEY,
-  `email` varchar(255),
-  `bio` varchar(255) DEFAULT '',
-  `profile` varchar(255) DEFAULT '',
-  `header` varchar(255) DEFAULT '',
-  `type` tinyint(1) COMMENT '0: User or 1: Admin'
-);
-
 CREATE TABLE `account` (
-  `uuid` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT '',
-  `username` varchar(255) DEFAULT '',
-  `contact` varchar(255) DEFAULT '',
-  `address` varchar(255) DEFAULT '',
-  `email` varchar(255) UNIQUE DEFAULT '',
-  `password` varchar(255) DEFAULT '',
-  `gender` varchar(255) DEFAULT '',
-  `birthday` date,
-  `created_at` timestamp,
-  `activated` tinyint(1)
-);
-
-CREATE TABLE `membership` (
-  `uuid` integer PRIMARY KEY,
-  `email` varchar(255) DEFAULT '',
-  `type` tinyint(1) COMMENT '0: Basic or 1: Advance',
-  `level` int DEFAULT 1,
-  `category` varchar(255) DEFAULT '',
-  `status` tinyint(1) DEFAULT 0,
-  `expiration` integer DEFAULT 0,
-  `subscription_date` integer DEFAULT 0
+  `uuid` int(11) PRIMARY KEY NOT NULL,
+  `name` varchar(255) DEFAULT null,
+  `username` varchar(255) DEFAULT null,
+  `email` varchar(255) DEFAULT null,
+  `password` varchar(255) DEFAULT null,
+  `gender` varchar(255) DEFAULT null,
+  `birthday` date DEFAULT null,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `activated` tinyint(1) DEFAULT null,
+  `contact` varchar(20) DEFAULT null,
+  `address` varchar(255) NOT NULL
 );
 
 CREATE TABLE `config` (
-  `uuid` integer PRIMARY KEY,
-  `email` varchar(255),
+  `uuid` int(11) DEFAULT null,
+  `email` varchar(255) DEFAULT null,
   `digital_clock` tinyint(1) DEFAULT 0,
   `birthday_year` tinyint(1) DEFAULT 0,
-  `sensetive_information` tinyint(1) DEFAULT 0
+  `sensitive_information` tinyint(1) DEFAULT 0
 );
 
-ALTER TABLE `account` ADD FOREIGN KEY (`uuid`) REFERENCES `membership` (`uuid`);
+CREATE TABLE `membership` (
+  `uuid` int(11) DEFAULT null,
+  `email` varchar(255) DEFAULT null,
+  `type` tinyint(1) DEFAULT null COMMENT '0: Basic or 1: Advance',
+  `level` int(11) DEFAULT 1,
+  `category` varchar(255) DEFAULT null,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `expiration` int(11) NOT NULL DEFAULT 0,
+  `subscription_date` int(11) NOT NULL DEFAULT 0
+);
 
-ALTER TABLE `account` ADD FOREIGN KEY (`uuid`) REFERENCES `user` (`uuid`);
+CREATE TABLE `user` (
+  `uuid` int(11) DEFAULT null,
+  `email` varchar(255) DEFAULT null,
+  `bio` varchar(255) DEFAULT null,
+  `profile` varchar(255) DEFAULT null,
+  `header` varchar(255) DEFAULT null,
+  `type` tinyint(1) DEFAULT null COMMENT '0: User or 1: Admin'
+);
 
-ALTER TABLE `account` ADD FOREIGN KEY (`uuid`) REFERENCES `config` (`uuid`);
+ALTER TABLE `config` ADD CONSTRAINT `config_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `account` (`uuid`);
+
+ALTER TABLE `membership` ADD CONSTRAINT `membership_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `account` (`uuid`);
+
+ALTER TABLE `user` ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `account` (`uuid`);
